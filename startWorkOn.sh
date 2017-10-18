c705f6a9cdb4ac9256ba1c73ff15f1b7180e3e5f
Tom Rees <tomrees@amazon.com>
2017-10-18 03:44:07 +0100

    Better dir structure.

--- startWorkOn.sh
+++ startWorkOn.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-echo "$(date -R) : $@" >> ~/timeTracking.md 
+TIME_TRACKING_DIR="/home/local/ANT/tomrees/time_tracking"
+
+current_date_dir=$TIME_TRACKING_DIR/$(date +"%Y/%m")
+mkdir -p $current_date_dir
+echo "$(date -R) : $@" >> $current_date_dir/timeTracking.md 
+
+
