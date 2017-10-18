dd36c8eec54ee5d75699191f475b6ddd3610f7fd
Tom Rees <tomrees@amazon.com>
2017-10-18 03:55:39 +0100

    Add day to filename

--- startWorkOn.sh
+++ startWorkOn.sh
@@ -3,6 +3,6 @@ TIME_TRACKING_DIR="/home/local/ANT/tomrees/time_tracking"
 
 current_date_dir=$TIME_TRACKING_DIR/$(date +"%Y/%m")
 mkdir -p $current_date_dir
-echo "$(date -R) : $@" >> $current_date_dir/timeTracking.md 
+echo "$(date -R) : $@" >> $current_date_dir/$(date +%d)-timeTracking.md 
 
 
